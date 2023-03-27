50da90a5fdff88085cbc3947f11d8b178cf4268c
Snuffy2 <Snuffy2@users.noreply.github.com>
2023-03-26 21:28:13 -0400

    Reworked Fix AttributeError on Restore

--- custom_components/variable/sensor.py
+++ custom_components/variable/sensor.py
@@ -142,19 +142,16 @@ class Variable(RestoreSensor):
                 # Unsure how to deal with state vs native_value on restore.
                 # Setting Restored state to override native_value for now.
                 # self._state = state.state
-                if sensor:
-                    if (
-                        state.state is not None
-                        and state.state.lower() != "none"
-                        and sensor.native_value != state.state
-                    ):
-                        _LOGGER.info(
-                            f"({self._attr_name}) Restored values are different. "
-                            f"native_value: {sensor.native_value} | state: {state.state}"
-                        )
-                        self._attr_native_value = state.state
-                else:
-                    self._state = state.state
+                if (sensor is None or (
+                    sensor and state.state is not None
+                    and state.state.lower() != "none"
+                    and sensor.native_value != state.state
+                )):
+                    _LOGGER.info(
+                        f"({self._attr_name}) Restored values are different. "
+                        f"native_value: {sensor.native_value} | state: {state.state}"
+                    )
+                    self._attr_native_value = state.state
 
     @property
     def should_poll(self):
